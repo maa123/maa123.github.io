94b56c035a9bb3775813f54cdd185842e91f02f0
maa123 <maa123.lq@gmail.com>
2017-10-29 15:35:32 +0900

    Update sl.js

--- js/sl.js
+++ js/sl.js
@@ -8,7 +8,7 @@ function nsc(at, am, nct) {
     }
 }
 
-window.addEventListener('DOMContentLoaded', function() {
+//window.addEventListener('DOMContentLoaded', function() {
     var ct = document.getElementById('footer').getBoundingClientRect().top;
     setTimeout(nsc, 10, (ct / 20), (ct % 20), 0);
-});
+//});
