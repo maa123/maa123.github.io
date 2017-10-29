cb9e9e65e7c42b341730ba444022a097a51bcfbc
maa123 <maa123.lq@gmail.com>
2017-10-29 15:23:45 +0900

    Create sl.js

---
+++ js/sl.js
@@ -0,0 +1,12 @@
+function nsc(at, am, nct) {
+    console.log(at);
+    if (nct < at) {
+        setTimeout(at, am, nct + 1);
+        scrollBy(0, 20);
+    } else {
+        scrollBy(0, am);
+    }
+}
+
+var ct = document.getElementById('footer').getBoundingClientRect().top;
+setTimeout(nsc, 10, (ct / 20), (ct % 20), 0);
