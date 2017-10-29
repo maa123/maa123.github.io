ed7613cbd984f11b5ee4bfa0750c38c5760ab5f6
maa123 <maa123.lq@gmail.com>
2017-10-29 15:42:20 +0900

    Update sl.js

--- js/sl.js
+++ js/sl.js
@@ -1,14 +1,3 @@
-function nsc(at, am, nct) {
-    console.log(at);
-    if (nct < at) {
-        setTimeout(at, am, nct + 1);
-        scrollBy(0, 20);
-    } else {
-        scrollBy(0, am);
-    }
-}
-
-//window.addEventListener('DOMContentLoaded', function() {
-    var ct = document.getElementById('footer').getBoundingClientRect().top;
-    setTimeout(nsc, 10, (ct / 20), (ct % 20), 0);
-//});
+jQuery("html,body").animate({
+            scrollTop: jQuery("#footer").offset().top
+        }, jQuery("#footer").offset().top / 2);
