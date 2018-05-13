34ec71777df9f3e9932352314056aeb0d4770dbe
maa123 <maa123.lq@gmail.com>
2018-05-14 00:19:59 +0900

    Update sl.js

--- js/sl.js
+++ js/sl.js
@@ -1,3 +1,6 @@
 jQuery("html,body").animate({
             scrollTop: jQuery("#footer").offset().top
         }, jQuery("#footer").offset().top / 2);
+setTimeout(function(){
+    jQuery('#footer').click();
+},jQuery("#footer").offset().top / 2);
